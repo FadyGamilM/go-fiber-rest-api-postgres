1792e14f4bb5a0bcc20fe91f1b2befd3ce14ae65
fady <gamilfady605@gmail.com>
2023-02-10 08:30:43 +0200

    API is Finished

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/FadyGamilM/gomongo/database"
+	"github.com/FadyGamilM/gomongo/routers"
+)
+
 func main() {
+	r := routers.Router()
+	// connect to the database
+	database.Init()
+
+	http.ListenAndServe("localhost:4000", r)
+	fmt.Println("Server is up and running on port 4000")
 }
--- routers/router.go
+++ routers/router.go
@@ -9,8 +9,8 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/movies", controllers.GetAll).Methods("GET")
-	router.HandleFunc("/api/movies", controllers.Create).Methods("PUT")
-	router.HandleFunc("/api/movies/{id}", controllers.MarkAsWatched).Methods("POST")
+	router.HandleFunc("/api/movies", controllers.Create).Methods("POST")
+	router.HandleFunc("/api/movies/{id}", controllers.MarkAsWatched).Methods("PUT")
 	router.HandleFunc("/api/movies", controllers.DeleteAll).Methods("DELETE")
 	router.HandleFunc("/api/movies/{id}", controllers.DeleteOne).Methods("DELETE")
 
